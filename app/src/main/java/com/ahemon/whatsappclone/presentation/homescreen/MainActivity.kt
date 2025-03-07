a48c20829ac4639dacb60d7dd6adb0267ea1bf41
MD AMRAN HOSSIN EMON <mdamranhossinemon78@domain.com>
2025-03-07 15:48:24 +0600

    initial Commit

---
+++ app/src/main/java/com/ahemon/whatsappclone/presentation/homescreen/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ahemon.whatsappclone.presentation.homescreen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ahemon.whatsappclone.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
